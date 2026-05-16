e4f21162167e16694d336dc7a4ae76e13a87139d
Rolando Barriga-Rojas <rbarrigarojas@gmail.com>
2026-05-15 22:46:28 -0700

    Add service worker for caching assets
    Implement service worker for caching and fetch handling

--- shorttask/service-worker.js
+++ shorttask/service-worker.js
@@ -1,1 +1,61 @@
+const CACHE_NAME = 'hybrid-report-cache-v1';
 
+const urlsToCache = [
+  './',
+  './index.html',
+  './styles.css'
+];
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys()
+      .then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+
+            return null;
+          })
+        );
+      })
+      .then(function() {
+        return self.clients.claim();
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(cachedResponse) {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(event.request)
+          .then(function(networkResponse) {
+            return networkResponse;
+          })
+          .catch(function() {
+            return caches.match('./index.html');
+          });
+      })
+  );
+});
